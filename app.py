02ca32760b02263fe2460c654d2453a072d4167d
giastahmad <giast360@gmail.com>
2026-03-22 15:51:35 +0700

    rebuild model machine learning to predict cause of anxiety

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-# CSS untuk styling (menggunakan CSS yang sudah ada, tidak ada perubahan)
+# CSS untuk styling
 st.markdown("""
 <style>
     [data-testid="stAppViewContainer"] {
